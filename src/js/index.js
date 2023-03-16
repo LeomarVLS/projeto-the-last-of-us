043461335d57e21767674fec95fe367b13d4c7d9
Leomar <112298240+LeomarVLS@users.noreply.github.com>
2023-03-15 22:08:37 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        desativarBotaoSelecionado();
+
+        selecionarBotaoCarrosel(botao);
+
+        escoderImagemAtiva();
+        
+        mostrarImagemDeFundo(indice);
+    })
+})
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function selecionarBotaoCarrosel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function escoderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
